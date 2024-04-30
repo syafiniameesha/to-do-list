4f6595c079b34319b4471e65e893280db7514394
Syafini Ameesha <syafiniameesha@gmail.com>
2024-04-30 16:04:26 +0800

    updated

--- to-do-list/src/App.vue
+++ to-do-list/src/App.vue
@@ -22,31 +22,7 @@ export default {
   data() {
     return {
       tasks: [
-        {
-          id: 1,
-          title: "Learn Vue JS",
-          completed: true,
-        },
-        {
-          id: 2,
-          title: "Watch netflix",
-          completed: true,
-        },
-        {
-          id: 3,
-          title: "Go shopping",
-          completed: false,
-        },
-        {
-          id: 4,
-          title: "Learn guitar",
-          completed: false,
-        },
-        {
-          id: 5,
-          title: "Send email",
-          completed: false,
-        },
+        //can insert temporary data here in json format
       ],
     };
   },
