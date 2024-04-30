54486536a119556c99db6e696081b1b7a6d4d9db
Syafini Ameesha <syafiniameesha@gmail.com>
2024-04-30 15:21:11 +0800

    updated

---
+++ to-do-list/src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="app">
+    <task-list
+      :tasks="tasks"
+      @taskAdded="handleTaskAdded"
+      @clearCompleted="handleClearCompleted"
+      @clearAll="handleClearAll"
+      @completeTask="handleCompleteTask"
+      @removeTask="handleRemoveTask"
+    ></task-list>
+  </div>
+</template>
+
+<script>
+import TaskList from "./components/Tasklist.vue";
+
+export default {
+  name: "App",
+  components: {
+    TaskList,
+  },
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          title: "Learn Vue JS",
+          completed: true,
+        },
+        {
+          id: 2,
+          title: "Watch netflix",
+          completed: true,
+        },
+        {
+          id: 3,
+          title: "Go shopping",
+          completed: false,
+        },
+        {
+          id: 4,
+          title: "Learn guitar",
+          completed: false,
+        },
+        {
+          id: 5,
+          title: "Send email",
+          completed: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    handleTaskAdded(task) {
+      this.tasks.push(task);
+    },
+    handleClearCompleted() {
+      this.tasks = this.tasks.filter(task => !task.completed);
+    },
+    handleClearAll() {
+      this.tasks = [];
+    },
+    handleCompleteTask(task) {
+      task.completed = !task.completed;
+    },
+    handleRemoveTask(index) {
+      this.tasks.splice(index, 1);
+    },
+  },
+};
+</script>
---
+++ to-do-list/src/components/DeletePopup.vue
@@ -0,0 +1,39 @@
+<template>
+    <div v-if="visible" class="popup-overlay">
+        <div class="popup">
+            <div class="popup-content">
+                <p class="popup-header-text">{{ message }}</p>
+                <p class="popup-header-msg">Are you sure to {{message}}? This action cannot be undone.</p>
+                <div class="margin-bottom"></div>
+                <div class="clearBtns">
+                    <button class="button-secondary" @click="cancel">Cancel</button>
+                    <button class="button-delete-primary" @click="confirm">Clear</button>
+                </div>
+            </div>
+        </div>
+  </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      visible: {
+        type: Boolean,
+        default: false
+      },
+      message: {
+        type: String,
+        default: 'Are you sure?'
+      }
+    },
+    methods: {
+      confirm() {
+        this.$emit('confirmed');
+      },
+      cancel() {
+        this.$emit('canceled');
+      }
+    }
+  };
+  </script>
+  
